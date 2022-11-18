6c79ce010108c6198762d9341430389e724ed153
Verjin V <verjin2001@gmail.com>
2022-11-18 22:57:48 +0530

    Sprint 2 Submission

---
+++ Project Development Phase/Sprint 2/static/JS/login.js
@@ -0,0 +1,25 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
